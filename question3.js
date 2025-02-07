cb0132ccea336fd6742a5aa5a8d5c1b5b9e2577a
Md. Shahid Raza <shahidhets1@gmail.com>
2025-02-07 11:48:14 +0530

    Delete question3.js

--- question3.js
+++
@@ -1,7 +0,0 @@
-let Total_budget = 15500;
-let department = 5;
-let percentage =[50,30,40,20,10];
-for(let i=0; i<department; i++){
-    Total_budget -= Total_budget * (percentage[i] / 100);
-    console.log("department=", percentage[i], "budget=", Total_budget);
-}
